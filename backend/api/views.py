f1932ec506bd6af2e1b723f4ec266ad55915c0e3
Julia Letova <julialikh@gmail.com>
2022-05-29 18:18:57 +0200

    Pep8

--- backend/api/views.py
+++ backend/api/views.py
@@ -5,7 +5,7 @@ from django.shortcuts import get_object_or_404
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import filters, status, viewsets
 from rest_framework.decorators import action, api_view, permission_classes
-from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
 from .filters import CustomFilter, IngredientSearchFilter
